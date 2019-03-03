3071d338034612f6da1579e2ad9b38a1e30a1c0a
fightinggg <42932982+fightinggg@users.noreply.github.com>
2019-03-03 21:19:32 +0800

    1
    1

---
+++ js/link.js
@@ -0,0 +1,17 @@
+// 背景
+var random = Math.round( Math.random() * 6 );
+if (window.innerWidth>1000){
+	document.write('<style>body{ background-image: url(/Myweb/img/'+random+'.jpg);!important;}</style>');
+ }
+
+
+document.writeln("   <link rel=\"stylesheet\" type=\"text/css\"  href=\"/fightinggg/css/head.css\">");
+document.writeln("   <link rel=\"stylesheet\" type=\"text/css\"  href=\"/fightinggg/css/pastebin.css\">");
+document.writeln("   <link rel=\"stylesheet\" type=\"text/css\"  href=\"/fightinggg/css/code.css\">");
+
+
+//自动提交
+document.writeln("    <script>(function(){var bp = document.createElement('script');var curProtocol = window.location.protocol.split(':')[0];if (curProtocol === 'https'){bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';}else{bp.src = 'http://push.zhanzhang.baidu.com/push.js';}var s = document.getElementsByTagName(\"script\")[0];s.parentNode.insertBefore(bp, s);})();</script>")
+
+
+
---
+++ js/up_and_right.js
@@ -0,0 +1,43 @@
+document.writeln('   <div id="up"> <center><a href="http://fightinggg.github.io/">逐    梦</a></center></div>																	');
+document.writeln('                   <nav id="menu" class="menu">     																																					');
+document.writeln('                   <div id="right">																																													');
+
+document.writeln('                   <div id="right_in">																																												');
+document.writeln('                        <h3><center> <a href="https://github.com/fightinggg/Myweb">github</a></br>	</center></h3>																																		');
+document.writeln('                        <hr>																																																');
+document.writeln('                                                    <a href="https://github.com/fightinggg/Myweb">github</a></br>													');
+document.writeln('                        <hr>																																																');
+document.writeln('                        </div>																																																');
+
+
+document.writeln('                   <div id="right_in">																																												');
+document.writeln('                        <h3><center>其他有趣的文章</center></h3>																																		');
+document.writeln('                        <hr>																																																');
+document.writeln('                                                    <a href="https://fightinggg.github.io/Myweb/" target="_blank">My Myweb Way</a></br>	');
+document.writeln('                                                    <a href="https://fightinggg.github.io/Computer_Architecture/" target="_blank">Computer_Architecture</a></br>					');
+document.writeln('                                                    <a href="https://fightinggg.github.io/SQL/" target="_blank">sql</a></br>																			');
+document.writeln('                                                    <a href="https://fightinggg.github.io/python_and_web/"  target="_blank">python and web</a></br>													');
+document.writeln('                        <hr>																																																');
+document.writeln('                        </div>																																																');
+
+
+document.writeln('                   <div id="right_in">																																												');
+document.writeln('                        <h3><center>珍藏网站</center></h3>																																		');
+document.writeln('                        <hr>																																																');
+document.writeln('                                                    <a href="https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html" target="_blank">gcc内建函数</a></br>	');
+document.writeln('                                                    <a href="https://www.codecogs.com/latex/eqneditor.php" target="_blank">数学公式</a></br>					');
+document.writeln('                                                    <a href="http://hzwer.com" target="_blank">hzwer</a></br>																			');
+document.writeln('                                                    <a href="http://www.jj20.com"  target="_blank">桌面高清背景图片</a></br>													');
+document.writeln('                        <hr>																																																');
+document.writeln('                        </div>																																																');
+
+document.writeln('                   <div id="right_in">																																												');
+document.writeln('                        <h3><center>友链</center></h3>																																				');
+document.writeln('                        <hr>																																																');
+document.writeln('                                                    <a href="https://blog.csdn.net/weixin_41863129" target="_blank">yg</a></br>											');
+document.writeln('                                                    <a href="https://decaku.github.io"  target="_blank">zwg</a></br>																	');
+document.writeln('                                                    <a href="https://www.cnblogs.com/chunibyo/"  target="_blank">wly</a></br>																	');
+document.writeln('                        <hr>																																																');
+document.writeln('                        </div>																																																');
+
+document.writeln('                    </div>																																																	');
