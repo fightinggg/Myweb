bf04d2f6c4c53eb73e04472fea167c555ce234d2
fightinggg <42932982+fightinggg@users.noreply.github.com>
2019-04-26 21:50:21 +0800

    阿斯顿发
    a s d f

--- js/web_end.js
+++ js/web_end.js
@@ -1,7 +1,7 @@
 
 document.writeln('                    </div>																																																	');
 document.writeln('                    </div>																																																	');
-document.writeln('                    <div id="down"><center>all right</center><center>如有问题请联系qq:1 1 4 4 5 6 0 5 5 3 </center><center><div style="width:80%"><hr></div></center><br></div>																																																');
+document.writeln('                    <div id="down"><center>all right</center><center>广告位招租 </center><center><div style="width:80%"><hr></div></center><br></div>																																																');
 document.writeln('                    </div>																																																	');
 document.writeln('                    </body>																																																	');
 
